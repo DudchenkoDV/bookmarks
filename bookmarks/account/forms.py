9206ecced233c3d00be6b35e7b7ce7e84ce9c3fd
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-09 12:43:10 +0300

    add def clean_password2

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -19,3 +19,9 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.changed_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords dont\'t match')
+        return cd['password2']
